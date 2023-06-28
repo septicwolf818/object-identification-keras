8d916945877edbb791dc1814e0dffb53f4ecdefd
Rafał Widło <rafal.widlo@inspirebrands.com>
2023-06-28 12:51:02 +0200

    Updated project name

--- ObjectIdentification/ObjectIdentification/asgi.py
+++ ObjectIdentification/ObjectIdentification/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ObjectIdentification project.
+ASGI config for objectidentification project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ObjectIdentification.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'objectidentification.settings')
 
 application = get_asgi_application()
--- ObjectIdentification/ObjectIdentification/urls.py
+++ ObjectIdentification/ObjectIdentification/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ObjectIdentification project.
+URL configuration for objectidentification project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
